ad64d94618fd764ce6f8d28873685ef15be6d085
Marise St-Pierre <naranjaqc@hotmail.com>
2020-07-27 14:48:31 -0400

    add new task form

---
+++ src/AddToDoForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class AddToDoForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {task: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        this.props.create(this.state);
+        this.setState({task:''});
+    }
+
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor='task'>New Task</label>
+                <input 
+                    type='text' 
+                    placeholder='New task' 
+                    id='task' 
+                    value={this.state.task}
+                    name='task'
+                    onChange={this.handleChange}
+                    />
+                <button>Add Task</button>
+            </form>
+        )
+    }
+}
+
+export default AddToDoForm;
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import ToDo from './ToDo';
+import AddToDoForm from './AddToDoForm';
 
 class ToDoList extends Component {
     constructor(props){
         super(props);
-        this.state = {todos: []}
+        this.state = {todos: [{task: 'Walk the fish'}, {task: 'Feed the Goblin'}]}
+        this.createNewTask = this.createNewTask.bind(this);
+    }
+
+    createNewTask(newTodo) {
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        })
     }
 
     render() {
+        const todos = this.state.todos.map(todo => {
+            return <ToDo task={todo.task} />
+        })
+
         return(
             <div>
                 <h1>Todo List</h1>
+                <AddToDoForm create={this.createNewTask}/>
                 <ul>
-                    <li>Todo 1</li>
-                    <li>Todo 2</li>
+                    {todos}
                 </ul>
             </div>
         )
